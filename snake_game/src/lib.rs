e338084591460bb1d3e839b3ec83bdaf48fa1d4c
ErikAnkerKilbergSkallevold <42532920+ErikAnkerKilbergSkallevold@users.noreply.github.com>
2022-09-19 21:39:36 +0200

    Self hosted, JS and Snake grid

---
+++ snake_game/src/lib.rs
@@ -0,0 +1,45 @@
+use wasm_bindgen::prelude::*;
+use wee_alloc::WeeAlloc;
+
+#[global_allocator]
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
+
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+}
+
+impl Snake {
+    fn new(spawn_index: usize) -> Self {
+        Snake {
+            body: vec!(SnakeCell(spawn_index))
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub struct World {
+    width: usize,
+    snake: Snake,
+}
+
+#[wasm_bindgen]
+impl World {
+    pub fn new(width: usize, snake: usize) -> Self {
+        Self {
+            width,
+            snake: Snake::new(snake),
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn snake_head_idx(&self) -> usize {
+        self.snake.body[0].0
+    }
+}
+
+// wasm-pack build --target web
